2af52cd2e6b8b8f5c0f2740ca05fcfd942138ce9
miggland <miggland@ipe.mavt.ethz.ch>
2011-12-12 10:38:32 +0100

    Example of how to use the PBE solver

---
+++ example.m
@@ -0,0 +1,35 @@
+%% Clean up
+
+clear
+clc
+close all
+
+%% Set up problem
+
+% Basic object
+PD = ProblemDefinition
+
+% Define initial conditions
+PD.init_dist.y = linspace(0,2);
+PD.init_dist.F = @(x) normalpdf(x,0.5,0.1,10);
+PD.init_conc = 1;
+
+% Define growth rate
+PD.growthrate = @(c,y)1e-2*ones(size(y));
+
+% Define solution parameters - time
+PD.sol_time = [0 100];
+
+%% Solve
+
+[t_out SolF Solc] = PBESolver(PD);
+
+%% Plot results
+
+% Plot distributions
+pls = plot(SolF);
+
+% Plot concentration
+plot(t_out,Solc)
+xlabel('Time')
+ylabel('Concentration')
