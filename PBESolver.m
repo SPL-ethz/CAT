8d3add6b6051ef242e6bf29072ac2f83b13fcc0e
Thomas Vetter <vettert@ethz.ch>
2012-01-13 11:14:02 +0100

    - HR now gives a volume vector, so that the plotting of the mass balance works.

--- PBESolver.m
+++ PBESolver.m
@@ -27,7 +27,7 @@ switch PD.sol_method
         SolutionTimes = output.time;
         SolutionConc = output.c;
         SolutionTemp = output.Temp;
-        SolutionVolume = PD.init_volume; % not implemented yet
+        SolutionVolume = PD.init_volume*ones(size(SolutionConc)); % not implemented yet
         
         for i = 1:length(SolutionTimes)
             SolutionDists(i) = Distribution(output.PSD.xp.dim1,output.PSD.F(:,i),output.PSD.xb.dim1);
