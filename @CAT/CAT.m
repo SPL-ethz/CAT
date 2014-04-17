cf4584ccb218d84449aa405565401b2b0dedb915
Martin Iggland <miggland@ipe.mavt.ethz.ch>
2014-04-17 13:32:49 +0200

    Move setDefaults to separate function

--- @CAT/CAT.m
+++ @CAT/CAT.m
@@ -127,24 +127,7 @@ classdef CAT < hgsetget
                 disp('Done.')
             end
             
-        end % function
-        
-        function setDefaults(O)
-            O.init_dist = Distribution(linspace(1,500,200),{'normal',100,20});
-            O.init_conc = 1; % saturated
-            O.solubility = @(T,xm) 1;
-            O.Tprofile = @(t) 25*ones(size(t));
-            O.ASprofile = @(t) 0*t;
-            O.growthrate = @(S,T,y) ones(size(y));
-            O.nucleationrate = @(S,T,m) 0;
-            O.init_seed = 1;
-            O.init_massmedium = 1000;
-            O.rhoc = 1e-12;
-            O.kv = 1;
-            O.sol_method = 'centraldifference'; 
-            O.sol_time = [0 100];
-        end
-        
+        end % function        
         
 %% SETTER AND GETTER METHODS
 
