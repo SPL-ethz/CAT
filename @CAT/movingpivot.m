b669ca8e832c775d129749e1b5be72ac21b86c9d
Martin Iggland <miggland@ipe.mavt.ethz.ch>
2014-04-17 16:57:30 +0200

    Change solve method: specialties in sub-method. Assing directly to output variables in the corresponding method (implemented for moving pivot so far)

--- @CAT/movingpivot.m
+++ @CAT/movingpivot.m
@@ -1,5 +1,9 @@
-function [dxdt] = movingpivot(O, t, x)
-%% [dxdt] = movingpivot(t, x, O) Moving Pivot method for Nucleation and Growth
+%% Main method movingpivot
+
+function movingpivot(O)
+
+% movingpivot
+%
 % Solves the PBE according to the Moving Pivot method (cf. e.g.  Kumar, S.; Ramkrishna, D. Chemical Engineering Science 1997, 52, 4659�4679.)
 % Needs to solve ODE's for number of particles (N), pivot length (y), boundaries (boundaries) and concentration,
 % i.e. the number of ODE's to be solved is 3*ngrid-2+1 and has an event listener.
@@ -11,6 +15,73 @@ function [dxdt] = movingpivot(O, t, x)
 % - Decrease reltol {1e-6} and abstol {1e-6} [sol_options]
 % - Reduce dL (criterion for bin addition) {10} [sol_options]
 % - Use another method
+
+if ~isempty(O.sol_options) && ~isempty(find(strcmpi(O.sol_options,'dL'),1))
+    dL = O.sol_options(find(strcmpi(O.sol_options,'dL'),1)+1);
+else
+    dL = 10; % critical bin size for event listener
+end
+X0 = [O.init_dist.F.*diff(O.init_dist.boundaries) ...
+    O.init_dist.y O.init_dist.boundaries O.init_conc];
+tstart = O.sol_time(1); % local start time
+tend = O.sol_time(end); % overall end time
+
+% if nucleation is present, bins are addded when the first bin
+% becomes too big, if dissolution is present, remove bins when
+% necessary
+options = O.sol_options;
+if isempty(O.sol_options)
+    options = odeset(options,'Events',@(t,x) EventBin(t,x,dL),'reltol',1e-6);
+else
+    options = odeset(options,'Events',@(t,x) EventBin(t,x,dL));
+end
+
+
+SolutionTimes = []; SolutionConc = [];
+s=0;
+while tstart<tend
+    ts = O.sol_time(O.sol_time > tstart & O.sol_time < tend);
+    
+    % Solve until the next event where the nucleation bin becomes to big (as defined by dL)
+    solvefun = @(t,x) movingpivot_ode(t,x,O);
+    [TIME,X_out] = ode15s(solvefun, [tstart ts tend],X0, options);
+    
+    X_out(X_out<0) = 0;
+    
+    nBins = (size(X_out,2)-2)/3;
+    
+    if X_out(end,2*nBins+1)>0
+        X0 = addBin(X_out(end,:)');
+    elseif X_out(end,nBins+1)<=0
+        X0 = removeBin(X_out(end,:)');
+    end
+    
+    F = X_out(:,1:nBins)./diff(X_out(:,2*nBins+1:3*nBins+1),1,2); F(isnan(F)) = 0;
+    
+    SolutionTimes = [SolutionTimes;TIME(:)]; %#ok<AGROW>
+    SolutionConc = [SolutionConc;X_out(:,end)]; %#ok<AGROW>
+    for i = 1:length(TIME)
+        SolutionDists(s+i) = Distribution( X_out(i,nBins+1:2*nBins),...
+            F(i,:),...
+            X_out(i,2*nBins+1:3*nBins+1) ); %#ok<AGROW>
+    end
+    s = s+length(TIME);
+    tstart = TIME(end);
+    
+    
+    
+end %while
+
+O.calc_time = SolutionTimes;
+O.calc_dist = SolutionDists;
+O.calc_conc = SolutionConc;
+
+end % function movingpivot
+
+function [dxdt] = movingpivot_ode(t, x, O)
+% [dxdt] = movingpivot(t, x, O) Moving Pivot method for Nucleation and Growth
+%
+% ODE function
 
 nBins = (length(x)-2)/3; % number of bins
 
--- @CAT/solve.m
+++ @CAT/solve.m
@@ -41,7 +41,7 @@ if ~isempty(O.tNodes)
 else    
     
     try
-        [O.calc_time, O.calc_dist, O.calc_conc] = PBESolver(O);
+        PBESolver(O);
     catch ME
         error('solve:tryconsttemp:PBESolverfail',...
             'PBESolver failed to integrate your problem. Message: %s',ME.message)
@@ -66,70 +66,18 @@ end
 
 end
 
-function [SolutionTimes SolutionDists SolutionConc] = PBESolver (O)
+function PBESolver (O)
 
 % PBESOLVER
 %
 % Handles individual solvers and returns (local) results
 
-solvefun = str2func(O.sol_method);
-solvefun = @(t,X) solvefun(t,X,O);
+solvefun = str2func(['@(t,X) ' O.sol_method '(t,X,O)']);
 
 switch O.sol_method
 
     case 'movingpivot'
-        if ~isempty(O.sol_options) && ~isempty(find(strcmpi(O.sol_options,'dL'),1))
-            dL = O.sol_options(find(strcmpi(O.sol_options,'dL'),1)+1);
-        else
-            dL = 10; % critical bin size for event listener
-        end
-        X0 = [O.init_dist.F.*diff(O.init_dist.boundaries) ...
-            O.init_dist.y O.init_dist.boundaries O.init_conc];
-        tstart = O.sol_time(1); % local start time
-        tend = O.sol_time(end); % overall end time
-
-        % if nucleation is present, bins are addded when the first bin
-        % becomes too big, if dissolution is present, remove bins when
-        % necessary
-        options = O.sol_options;
-        if isempty(O.sol_options)
-            options = odeset(options,'Events',@(t,x) EventBin(t,x,dL),'reltol',1e-6);            
-        else
-            options = odeset(options,'Events',@(t,x) EventBin(t,x,dL));
-        end
-
-        
-        SolutionTimes = []; SolutionConc = [];
-        s=0;
-        while tstart<tend 
-            ts = O.sol_time(O.sol_time > tstart & O.sol_time < tend);
-            
-            % Solve until the next event where the nucleation bin becomes to big (as defined by dL)
-            [TIME,X_out] = ode15s(solvefun, [tstart ts tend],X0, options);
-            
-            X_out(X_out<0) = 0;
-            
-            nBins = (size(X_out,2)-2)/3;
-            
-            if X_out(end,2*nBins+1)>0
-                X0 = addBin(X_out(end,:)'); 
-            elseif X_out(end,nBins+1)<=0
-                X0 = removeBin(X_out(end,:)'); 
-            end
-                                   
-            F = X_out(:,1:nBins)./diff(X_out(:,2*nBins+1:3*nBins+1),1,2); F(isnan(F)) = 0;
-            
-            SolutionTimes = [SolutionTimes;TIME(:)]; %#ok<AGROW>
-            SolutionConc = [SolutionConc;X_out(:,end)]; %#ok<AGROW>
-            for i = 1:length(TIME)
-                SolutionDists(s+i) = Distribution( X_out(i,nBins+1:2*nBins),...
-                    F(i,:),...
-                    X_out(i,2*nBins+1:3*nBins+1) ); %#ok<AGROW>
-            end
-            s = s+length(TIME);
-            tstart = TIME(end); 
-            
-        end %while        
+        O.movingpivot;       
         
     case 'centraldifference'
         
@@ -151,7 +99,7 @@ end %switch
 % For CD and HR transform result-arrays into appropriate output structure
 if ~strcmpi(O.sol_method,'movingpivot')
     SolutionConc = X_out(:,end);
-    SolutionDists = repmat(Distribution(),1,length(SolutionTimes));  %# Pre-Allocation for speed               
+    SolutionDists = repmat(Distribution(),1,length(SolutionTimes));  % Pre-Allocation for speed               
     for i = 1:length(SolutionTimes)
             SolutionDists(i) = Distribution( O.init_dist.y, X_out(i,1:length(O.init_dist.y)),O.init_dist.boundaries );
     end % for
