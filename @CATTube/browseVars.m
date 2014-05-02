2bc7944a0f7c8451a8a90ea267a4f36507fcb6b3
Dave Ochsenbein <daveo@ethz.ch>
2014-05-02 15:25:48 +0200

    everything seems to work

--- @CATTube/browseVars.m
+++ @CATTube/browseVars.m
@@ -11,9 +11,9 @@ if nargin < 6 || isempty(classfilter)
 else
     % Check if classfilter is char or cell - convert to cell
     if ischar(classfilter)
-        classfilter = {classfilter};
+        classfilter = {classfilter 'struct'};
     else
-        classfilter = [classfilter];
+        classfilter = [classfilter 'struct'];
     end % if
 end % if else
 
@@ -63,13 +63,18 @@ glb.import = uicontrol(glb.fighandle,...
     'Callback',@(hObject,Event)updateVarList(hObject,Event),...
     'Position',[280 260 110 30]);
 
-    function V = getVarList
+    function [V,Vvis] = getVarList(~,~,unHideSwitch)
+        
+        if nargin<3
+            unHideSwitch = [];
+        end
         
         % Get variable list
         varList = evalin('base','who');
         varList = varList(~strcmp('varList',varList) & ~strcmp('activeList',varList));
-        varList = [varList repmat({''},size(varList))];
+        varList = [varList repmat({''},size(varList)) num2cell([1:length(varList)]') repmat({0},size(varList))];
         activeList = varList;
+        id = length(varList(:,1));
         % if there are structures, whose fields may contain the necessary
         % data, we should be aware of this... therefore let's first explore
         % all structures in the workspace
@@ -77,16 +82,19 @@ glb.import = uicontrol(glb.fighandle,...
             if isa(evalin('base',[activeList{1,2},activeList{1,1}]),'struct')
                 for i = 1:length(evalin('base',[activeList{1,2},activeList{1,1}]))
                     if length(evalin('base',[activeList{1,2},activeList{1,1}]))>1
+                        li = length(evalin('base',[activeList{1,2},activeList{1,1}]));
                         indstr = ['(',num2str(i),')'];
                     else
+                        li = 1;
                         indstr = '';
                     end
                     fields = fieldnames(evalin('base',[activeList{1,2},activeList{1,1},indstr]));
-                    varList = [varList; fields repmat({[activeList{1,2},activeList{1,1},indstr,'.']},[length(fields) 1])];
-                    activeList = [activeList; fields repmat({[activeList{1,2},activeList{1,1},indstr,'.']},[length(fields) 1])];
+                    varList = [varList; fields repmat({[activeList{1,2},activeList{1,1},indstr,'.']},[length(fields) 1]) num2cell(id+(1:length(fields))') repmat({activeList{1,3}+(i-1)*1/li},[length(fields) 1])];
+                    activeList = [activeList; fields repmat({[activeList{1,2},activeList{1,1},indstr,'.']},[length(fields) 1]) num2cell(id+(1:length(fields))') repmat({activeList{1,3}+(i-1)*1/li},[length(fields) 1])];
+                    id = id+length(fields);
                 end
             end
-
+%             id = id +1;
             activeList(1,:) = [];
 
         end
@@ -94,47 +102,141 @@ glb.import = uicontrol(glb.fighandle,...
         
         % If filters defined, keep only those class types listed
         if ~isempty(classfilter)
-            Vname = cell(0);
+            Vname = cell(0);Vidparent = zeros(0,2);
             for i = 1:length(classfilter)
                 for j = 1:length(varList(:,1))
                    if isa(evalin('base',[varList{j,2},varList{j,1}]),classfilter{i})
                        Vname = [Vname;[varList{j,2},varList{j,1}]];
+                       Vidparent = [Vidparent; varList{j,3} varList{j,4}];
                    end
                 end
             end % for
         else
-            Vname = cell(0);
+            Vname = cell(0);Vidparent = zeros(0,2);
             for j = 1:length(varList(:,1))
                 Vname = [Vname;[varList{2,j},varList{1,j}]];
+                Vidparent = [Vidparent; varList{j,3} varList{j,4}];
             end
         end % if
         
-        parents = strtok(Vname,'.');
-        [~,I] = sort(parents);
-        Vname = Vname(I);
-        
+%         parents = strtok(Vname,'.');
+%         [~,I] = sort(parents);
+%         
+        parents = Vidparent(Vidparent(:,end) == 0,1);
+        Inew = parents(:)';
+        while ~isempty(parents)
+            children = Vidparent(parents(1)==floor(Vidparent(:,2)),:);
+            children = sortrows(children,2);
+            children = children(:,1);
+            I = find(Inew==parents(1));
+            Iloc = Inew(1:I);
+            Inew(1:I) = [];
+            Iloc = [Iloc children(:)' Inew];
+            Inew = Iloc;
+            parents = [parents children(:)'];
+            parents(1) = [];
+        end
+        for i = 1:length(Inew)
+            Vname2{i} = Vname{Vidparent(:,1) == Inew(i)};
+            Vidparent2(i,:) = Vidparent(Vidparent(:,1) == Inew(i),:);
+        end
+        Vname = Vname2(:);
+        Vidparent = Vidparent2;
 %         keyboard
+        
+        
+        V = struct('name',[],'class',[],'size',[],'depth',[],'id',[],'parent',[],'hiddenFlag',[]);
+        
         for i = 1:length(Vname)
            V(i).name = Vname{i};
+           if ~isfield(glb,'V') && any(Vidparent(:,2)==Vidparent(i,1))
+              V(i).name = [V(i).name,'+']; 
+           elseif isfield(glb,'V')
+               V(i).name = glb.V(i).name;
+           end
            V(i).class = class(evalin('base',Vname{i}));
            V(i).size = size(evalin('base',Vname{i}));
-           V(i).depth = max([0 length(strfind(Vname{i},'.'))-1]);
+           V(i).depth = length(strfind(Vname{i},'.'));
+           V(i).id = Vidparent(i,1);
+           V(i).parent = Vidparent(i,2);
+           
+           
+           if ~isfield(glb,'V') && V(i).depth==0
+               V(i).hiddenFlag = 0;
+           elseif ~isfield(glb,'V') && V(i).depth>0
+               V(i).hiddenFlag = 1;
+           else
+               V(i).hiddenFlag = glb.V(i).hiddenFlag;
+           end
         end
         
+        j = 1;
+        for i = 1:length(Vname)
+            if ~V(i).hiddenFlag
+                Vvis(j) = V(i);
+                j = j+1;
+            end
+        end
+        
+        for i = 1:length(Vvis)
+           if any(unHideSwitch==i)
+               if ~isempty(strfind(V([V.id]==Vvis(i).id).name,'+'))
+                    V([V.id]==Vvis(i).id).name = strrep(V([V.id]==Vvis(i).id).name,'+','-');
+               else
+                    V([V.id]==Vvis(i).id).name = strrep(V([V.id]==Vvis(i).id).name,'-','+');
+               end
+
+               for jj = 1:length(V)
+                   if floor(V(jj).parent) == Vvis(i).id
+                    V(jj).hiddenFlag = ~glb.V(jj).hiddenFlag;
+                   end
+               end
+           end
+        end
+        
+        for i = 1:length(V)
+           if V(i).parent ~=0 && V([V.id]==floor(V(i).parent)).hiddenFlag == 1
+               V(i).hiddenFlag = 1;
+               V(i).name = strrep(V(i).name,'-','+');
+           end
+        end
+        
+        for i = 1:length(V)
+           if V(i).parent ~=0 && (V([V.id] == floor(V(i).parent)).hiddenFlag ==1 || V(i).hiddenFlag == 1)
+               V(i).name = strrep(V(i).name,'-','+');
+           end
+        end
+        
+        j = 1;
+        for i = 1:length(Vname)
+            if ~V(i).hiddenFlag
+                Vvis(j) = V(i);
+                j = j+1;
+            end
+        end
         
     end % function getVarList
 
-    function updateVarList(~,~)
+    function updateVarList(~,~,unHideSwitch)
         
-        glb.V = getVarList;
+        if nargin<3
+           unHideSwitch = []; 
+        end
+        
+        [glb.V,glb.Vvis] = getVarList([],[],unHideSwitch);
         
         
         varnames = [{glb.V.name}']; %#ok<NBRAK>
+        j = 1;
+        finalvarnames = cell(0);
         for i =1:length(varnames)
-           varnames{i} = [repmat('  ',[1 glb.V(i).depth]),varnames{i}];
+           if ~glb.V(i).hiddenFlag
+            finalvarnames{j} = [repmat('  ',[1 glb.V(i).depth]),varnames{i}];
+            j = j+1;
+           end
         end
         
-        set(glb.lbox,'String',varnames);
+        set(glb.lbox,'String',finalvarnames);
         
     end % function updateVarList
 
@@ -148,31 +250,23 @@ glb.import = uicontrol(glb.fighandle,...
         % a
         % 2x3 double
         
-        vname = sprintf('%s\n',glb.V(varnum).name);
+        vname = sprintf('%s\n',glb.Vvis(varnum).name);
         if length(glb.V(varnum).size) == 2
-            vsize = sprintf('%ix%i',glb.V(varnum).size);
+            vsize = sprintf('%ix%i',glb.Vvis(varnum).size);
         else %multidimensional
-            vsize = sprintf('%ix%ix...',glb.V(varnum).size(1:2));
+            vsize = sprintf('%ix%ix...',glb.Vvis(varnum).size(1:2));
         end % if else
         
-        vtext = sprintf('%s %s %s',vname,vsize,glb.V(varnum).class);
+        vtext = sprintf('%s %s %s',vname,vsize,glb.Vvis(varnum).class);
         
         % Print variable details
-        set(glb.text,'String',vtext)
+        set(glb.text,'String',vtext);
         
-%         if (strcmp(get(glb.fighandle, 'SelectionType'), 'open')) % if double click
-%             expandStruct(hObject,[])
-%         end
+        if (strcmp(get(glb.fighandle, 'SelectionType'), 'open')) % if double click
+            updateVarList([],[],get(glb.lbox,'value'));
+        end
         
     end % function
-
-%     function expandStruct(hObject,~)
-%         strind = get(glb.lbox,'value');
-%         strname = structInfos.names{structInfos.values == strind};
-%         fields = fieldnames(evalin('base',strname));
-%         updateVarList(hObject,[],fields);
-%         keyboard
-%     end
 
     function setVar(hObject,~)
         
