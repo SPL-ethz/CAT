93bcc9d71211dab9084e0f6b9f9a6007d9a451a3
Martin Iggland <miggland@ipe.mavt.ethz.ch>
2014-05-06 16:17:55 +0200

    Remove hgsetgetplus class

--- @hgsetgetplus/Contents.m
+++
@@ -1,9 +0,0 @@
-% @HGSETGETPLUS
-%
-% Files
-%   configToString   - Convert a structure containing validation information to a structure
-%   configure        - Return validation criteria for SET.
-%   configureDefault - Make sure that the attributes structure has all required
-%   hgsetgetplus     - HG-style set and get for MATLAB objects. Alternative
-%   setdisp          - Specialized MATLAB object property display.
-%   setOneProperty   - Set a single property using a name/value pair.
