49c6b9b3f33ef3976c1f6a672f34b362062c6c02
Dave Ochsenbein <daveo@ethz.ch>
2015-05-26 08:54:13 +0200

    overwrite plots checkbox in CATTube
    Additionally some stuff for the browsing

--- @CATTube/solve.m
+++ @CATTube/solve.m
@@ -8,7 +8,8 @@ set(O.gui.run.start,'Enable','off')
 % Run the real solve method
 solve@CAT(O);
 
-% Activate the plot button
+% Activate the plot button and the overwrite checkbox
 set(O.gui.run.plot,'Enable','on');
+set(O.gui.run.plot_overwrite,'Enable','on');
 
 end % function
